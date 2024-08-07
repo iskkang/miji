5788c544d9d2c3c154c9894969a887c2cb7e4ca0
iskkang <76502863+iskkang@users.noreply.github.com>
2024-08-07 18:48:16 +0900

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 async function fetchData(endpoint) {
-    const BASE_URL = 'https://port-0-mclo-lysc4ja0acad2542.sel4.cloudtype.app/';
+    const BASE_URL = 'https://mclab.onrender.com/';
     try {
         const response = await fetch(`${BASE_URL}${endpoint}`, {
             headers: {

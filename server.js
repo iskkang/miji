4ee1846ea3e00d9fa79c5991e74787892b138eb8
iskkang <76502863+iskkang@users.noreply.github.com>
2024-08-30 14:57:01 +0900

    Update server.js

--- server.js
+++ server.js
@@ -48,8 +48,12 @@ async function fetchArticles() {
     const url = 'https://www.haesainfo.com/news/articleList.html?sc_section_code=S1N2&view_type=sm';
     
     try {
-        const { data } = await axios.get(url);
-        const $ = cheerio.load(data);
+    const { data } = await axios.get(url, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+      }
+    });
+    const $ = cheerio.load(data);
     
         const articles = [];
     

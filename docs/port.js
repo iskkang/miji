daf1a307f1c26db7a3efbb93dec2881cba4dadc5
iskkang <76502863+iskkang@users.noreply.github.com>
2024-08-31 23:35:05 +0900

    Update port.js

--- docs/port.js
+++ docs/port.js
@@ -1,68 +1,72 @@
 const axios = require('axios');
 
-// 포트 이름과 URL 매핑
-const portUrls = {
-    SGN: 'https://www.econdb.com/maritime/ports/async/SG%20SIN/',
-    SHA: 'https://www.econdb.com/maritime/ports/async/CN%20SHA/',
-    PUS: 'https://www.econdb.com/maritime/ports/async/KR PUS',
-    NGB: 'https://www.econdb.com/maritime/ports/async/CN NGB',
-    RTM: 'https://www.econdb.com/maritime/ports/async/NL RTM',
-    JEA: 'https://www.econdb.com/maritime/ports/async/AE JEA',
-    TAO: 'https://www.econdb.com/maritime/ports/async/CN TAO',
-    TPP: 'https://www.econdb.com/maritime/ports/async/MY TPP',
-    ANR: 'https://www.econdb.com/maritime/ports/async/BE ANR',
-    KHH: 'https://www.econdb.com/maritime/ports/async/TW KHH',
-    PTM: 'https://www.econdb.com/maritime/ports/async/MA PTM',
-    PKG: 'https://www.econdb.com/maritime/ports/async/MY PKG',
-    HKG: 'https://www.econdb.com/maritime/ports/async/HK HKG',
-    SHK: 'https://www.econdb.com/maritime/ports/async/CN SHK',
-    LAX: 'https://www.econdb.com/maritime/ports/async/US LAX',
-    LCH: 'https://www.econdb.com/maritime/ports/async/TH LCH',
-    YTN: 'https://www.econdb.com/maritime/ports/async/CN YTN',
-    LGB: 'https://www.econdb.com/maritime/ports/async/US LGB',
-    SGN_VN: 'https://www.econdb.com/maritime/ports/async/VN SGN',
-    HAM: 'https://www.econdb.com/maritime/ports/async/DE HAM',
-    NYC: 'https://www.econdb.com/maritime/ports/async/US NYC',
-    MUN: 'https://www.econdb.com/maritime/ports/async/IN MUN',
-    XMN: 'https://www.econdb.com/maritime/ports/async/CN XMN',
-    NSA_IN: 'https://www.econdb.com/maritime/ports/async/IN NSA',
-    ALG: 'https://www.econdb.com/maritime/ports/async/ES ALG',
-    CMB: 'https://www.econdb.com/maritime/ports/async/LK CMB',
-    HPH: 'https://www.econdb.com/maritime/ports/async/VN HPH',
-    ONX: 'https://www.econdb.com/maritime/ports/async/PA ONX',
-    VLC: 'https://www.econdb.com/maritime/ports/async/ES VLC',
-    SSZ: 'https://www.econdb.com/maritime/ports/async/BR SSZ',
-    BRY: 'https://www.econdb.com/maritime/ports/async/DE BRY',
-    JKT: 'https://www.econdb.com/maritime/ports/async/ID JKT',
-    TXG: 'https://www.econdb.com/maritime/ports/async/CN TXG',
-    BLB: 'https://www.econdb.com/maritime/ports/async/PA BLB',
-    PIR: 'https://www.econdb.com/maritime/ports/async/GR PIR',
-    SAV: 'https://www.econdb.com/maritime/ports/async/US SAV',
-    NSA_CN: 'https://www.econdb.com/maritime/ports/async/CN NSA',
-    PSD: 'https://www.econdb.com/maritime/ports/async/EG PSD',
-    HOU: 'https://www.econdb.com/maritime/ports/async/US HOU',
-    VUT: 'https://www.econdb.com/maritime/ports/async/VN VUT',
-};
+const portData = [
+  { name: 'SGN', url: 'https://www.econdb.com/maritime/ports/async/SG%20SIN/' },
+  { name: 'SHA', url: 'https://www.econdb.com/maritime/ports/async/CN%20SHA/' },
+  { name: 'PUS', url: 'https://www.econdb.com/maritime/ports/async/KR%20PUS/' },
+  { name: 'NGB', url: 'https://www.econdb.com/maritime/ports/async/CN%20NGB/' },
+  { name: 'RTM', url: 'https://www.econdb.com/maritime/ports/async/NL%20RTM/' },
+  { name: 'JEA', url: 'https://www.econdb.com/maritime/ports/async/AE%20JEA/' },
+  { name: 'TAO', url: 'https://www.econdb.com/maritime/ports/async/CN%20TAO/' },
+  { name: 'TPP', url: 'https://www.econdb.com/maritime/ports/async/MY%20TPP/' },
+  { name: 'ANR', url: 'https://www.econdb.com/maritime/ports/async/BE%20ANR/' },
+  { name: 'KHH', url: 'https://www.econdb.com/maritime/ports/async/TW%20KHH/' },
+  { name: 'PTM', url: 'https://www.econdb.com/maritime/ports/async/MA%20PTM/' },
+  { name: 'PKG', url: 'https://www.econdb.com/maritime/ports/async/MY%20PKG/' },
+  { name: 'HKG', url: 'https://www.econdb.com/maritime/ports/async/HK%20HKG/' },
+  { name: 'SHK', url: 'https://www.econdb.com/maritime/ports/async/CN%20SHK/' },
+  { name: 'LAX', url: 'https://www.econdb.com/maritime/ports/async/US%20LAX/' },
+  { name: 'LCH', url: 'https://www.econdb.com/maritime/ports/async/TH%20LCH/' },
+  { name: 'YTN', url: 'https://www.econdb.com/maritime/ports/async/CN%20YTN/' },
+  { name: 'LGB', url: 'https://www.econdb.com/maritime/ports/async/US%20LGB/' },
+  { name: 'SGN', url: 'https://www.econdb.com/maritime/ports/async/VN%20SGN/' },
+  { name: 'HAM', url: 'https://www.econdb.com/maritime/ports/async/DE%20HAM/' },
+  { name: 'NYC', url: 'https://www.econdb.com/maritime/ports/async/US%20NYC/' },
+  { name: 'MUN', url: 'https://www.econdb.com/maritime/ports/async/IN%20MUN/' },
+  { name: 'XMN', url: 'https://www.econdb.com/maritime/ports/async/CN%20XMN/' },
+  { name: 'NSA', url: 'https://www.econdb.com/maritime/ports/async/IN%20NSA/' },
+  { name: 'ALG', url: 'https://www.econdb.com/maritime/ports/async/ES%20ALG/' },
+  { name: 'CMB', url: 'https://www.econdb.com/maritime/ports/async/LK%20CMB/' },
+  { name: 'HPH', url: 'https://www.econdb.com/maritime/ports/async/VN%20HPH/' },
+  { name: 'ONX', url: 'https://www.econdb.com/maritime/ports/async/PA%20ONX/' },
+  { name: 'VLC', url: 'https://www.econdb.com/maritime/ports/async/ES%20VLC/' },
+  { name: 'SSZ', url: 'https://www.econdb.com/maritime/ports/async/BR%20SSZ/' },
+  { name: 'BRY', url: 'https://www.econdb.com/maritime/ports/async/DE%20BRY/' },
+  { name: 'JKT', url: 'https://www.econdb.com/maritime/ports/async/ID%20JKT/' },
+  { name: 'TXG', url: 'https://www.econdb.com/maritime/ports/async/CN%20TXG/' },
+  { name: 'BLB', url: 'https://www.econdb.com/maritime/ports/async/PA%20BLB/' },
+  { name: 'PIR', url: 'https://www.econdb.com/maritime/ports/async/GR%20PIR/' },
+  { name: 'SAV', url: 'https://www.econdb.com/maritime/ports/async/US%20SAV/' },
+  { name: 'NSA', url: 'https://www.econdb.com/maritime/ports/async/CN%20NSA/' },
+  { name: 'PSD', url: 'https://www.econdb.com/maritime/ports/async/EG%20PSD/' },
+  { name: 'HOU', url: 'https://www.econdb.com/maritime/ports/async/US%20HOU/' },
+  { name: 'VUT', url: 'https://www.econdb.com/maritime/ports/async/VN%20VUT/' }
+];
 
-// JSON 데이터를 가져오는 함수
-async function fetchData(portName) {
-    const url = portUrls[portName];
-    if (!url) {
-        throw new Error(`No URL found for port name: ${portName}`);
+async function fetchPortData(portName) {
+  try {
+    const port = portData.find(p => p.name === portName);
+    if (!port) {
+      throw new Error(`Port ${portName} not found`);
     }
 
-    try {
-        const response = await axios.get(url, {
-            headers: {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-                "Accept": "application/json"
-            }
-        });
-        return response.data;
-    } catch (error) {
-        console.error(`Error fetching data for port ${portName}:`, error);
-        throw error;
-    }
+    const response = await axios.get(port.url);
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching data for port ${portName}:`, error.message);
+    return null;
+  }
 }
 
-module.exports = { portData };
+async function fetchAllPortsData() {
+  const results = await Promise.all(portData.map(async (port) => {
+    const data = await fetchPortData(port.name);
+    return { name: port.name, data };
+  }));
+  return results.filter(result => result.data !== null);
+}
+
+module.exports = {
+  fetchPortData,
+  fetchAllPortsData
+};
